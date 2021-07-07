33fb5846d778212e99aed0d3aac1626039ace8fc
WingLim <winglim@qq.com>
2021-07-07 22:39:56 +0800

    feat: world diarys selector

--- constant/selector/selector.go
+++ constant/selector/selector.go
@@ -5,3 +5,13 @@ const (
 	CSRFTOKEN  = "meta[name=csrf-token]"
 	WORLD      = "body > div.container.below-top-navbar > div > div.box-head > h2"
 )
+
+const (
+	WorldDiarys   = "ul.posts-list li.wu"
+	DiaryId       = "div.meta > a.timeago"
+	DiaryNickname = "div.line > a"
+	DiaryTitle    = "div.line > h4 > a"
+	DiaryContent  = "div.line > div.comment"
+	DiaryLocation = "div.line > span.location"
+	DiaryDate     = "div.meta > a.timeago > time"
+)
