0d4751a86e9fb25fe92628ac766b610f0c78336a
WingLim <winglim@qq.com>
2021-07-09 10:53:46 +0800

    refactor: split out selector to files

---
+++ constant/selector/conversations_selector.go
@@ -0,0 +1,13 @@
+package selector
+
+const (
+	ConversationsList           = "ul.conversations-list li"
+	ConversationItemID          = "li > a"
+	ConversationItemUser        = "span.user"
+	ConversationItemLastMessage = "div.line"
+	ConversationItemLastDate    = "time.timeago"
+
+	MoreMessages        = "div.more-messages > a"
+	Conversations       = "ul.messages-list li"
+	ConversationContent = "div.box"
+)
---
+++ constant/selector/diary_selector.go
@@ -0,0 +1,14 @@
+package selector
+
+const (
+	WorldDiaries  = "ul.posts-list li.wu"
+	DiaryId       = "a.timeago"
+	DiaryNickname = "a.user"
+	DiaryTitle    = "div.line > h4 > a"
+	DiaryContent  = "div.comment"
+	DiaryLocation = "span.location"
+	DiaryDate     = "time.timeago"
+
+	Comments    = "div.cntr > div > div.cntr > ul > li.wu"
+	CommentDate = "time.hours"
+)
