ce45645ee05a651fc14da854725d999a43813d5d
WingLim <winglim@qq.com>
2021-07-09 10:52:28 +0800

    refactor: split out path to files

--- constant/path/path.go
+++ constant/path/path.go
@@ -9,34 +9,7 @@ const (
 	World   = "/world"
 )
 
-// Diary Url Path
-const (
-	NewDiary    = "/activities?act_id=301&re_id=icty%2Buser%2B1irqw29"
-	DeleteDiary = "/a/%s?act_id=301&re_id=icty%%2Buser%%2B1irqw29"
-
-	Like   = "/a/%s/like"
-	Unlike = "/a/%s/unlike"
-
-	DiaryPrivacy = "/a/%s/set_privacy?user_privacy=%d"
-
-	NewComment    = "/activities?act_id=101&re_id=icty%%2Bactivity%%2B%s"
-	DeleteComment = "/a/%s?act_id=101&re_id=icty%%2Bactivity%%2B%s"
-	GetComments   = "/a/%s/expand"
-)
-
 const (
 	Follow   = "/u/%s/follow"
 	Unfollow = "/u/%s/unfollow"
-)
-
-const (
-	SettingsIndex = "/settings/user/index"
-	Social        = "/settings/user/social"
-	Privacy       = "/settings/user/privacy"
-)
-
-const (
-	Conversations = "/user/conversations"
-	Conversation  = "/user/conversations/%s"
-	SendMessage   = "/user/conversations/%s/po"
 )
